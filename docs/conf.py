83a77da21124438149d87364c6db2be779005302
Doug Latornell <djl@douglatornell.ca>
2013-10-27 17:33:36 -0700

    Add bathy_tools module, sub-domain bathy notebook, and bathy docs section.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../bathymetry'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -27,6 +27,7 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
