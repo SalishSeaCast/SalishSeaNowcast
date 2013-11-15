c095db5befd36c541a81326e7fe836d282ead96b
Doug Latornell <djl@douglatornell.ca>
2013-11-15 09:12:26 -0800

    Add nc_tools module: functions for exploring & managing netCDF file attrs.

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ import mock
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../bathymetry'))
+sys.path.insert(0, os.path.abspath('../SalishSeaTools/salishsea_tools'))
 
 # -- General configuration -----------------------------------------------------
 
