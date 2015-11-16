a6ecb697bb50f417229cf82bb557f0660b2db990
Doug Latornell <djl@douglatornell.ca>
2015-11-16 10:52:04 -0500

    SalishSeaNowcast is a Python 3 package.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import __pkg_metadata__
 
 python_classifiers = [
     'Programming Language :: Python :: {0}'.format(py_version)
-    for py_version in ['2', '2.7', '3', '3.4', '3.5']]
+    for py_version in ['3', '3.4', '3.5']]
 other_classifiers = [
     'Development Status :: ' + __pkg_metadata__.DEV_STATUS,
     'License :: OSI Approved :: Apache Software License',
