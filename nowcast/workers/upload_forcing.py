b21ad43882d42b0b93e2b72982877c918788198a
Susan Allen <sallen@eoas.ubc.ca>
2017-02-05 09:05:35 -0800

    Correct how far into the future we have Live Ocean files.

--- nowcast/workers/upload_forcing.py
+++ nowcast/workers/upload_forcing.py
@@ -148,7 +148,7 @@ def upload_forcing(parsed_args, config, *args):
             host_run_config['forcing']['weather dir'], dest_dir, filename)
         _upload_file(sftp_client, host_name, localpath, remotepath)
     # Live Ocean Boundary Conditions
-    for day in range(0, 3):
+    for day in range(0, 2):
         filename = config['temperature salinity']['file template'].format(
             run_date.replace(days=day).date())
         dest_dir = '' if day == 0 else 'fcst'
