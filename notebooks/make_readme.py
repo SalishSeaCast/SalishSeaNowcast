fde615f320f1bbd67a7381f154e8f64b2ca025d3
Doug Latornell <djl@douglatornell.ca>
2017-02-17 14:48:27 -0800

    Update copyright year range to include 2017.

--- notebooks/make_readme.py
+++ notebooks/make_readme.py
@@ -1,7 +1,7 @@
 """Salish Sea NEMO Jupyter Notebook collection README generator
 
 
-Copyright 2013-2016 The Salish Sea MEOPAR Contributors
+Copyright 2013-2017 The Salish Sea MEOPAR Contributors
 and The University of British Columbia
 
 Licensed under the Apache License, Version 2.0 (the "License");
