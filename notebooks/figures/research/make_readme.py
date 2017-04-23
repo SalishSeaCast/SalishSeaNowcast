97c265f77593c6822f11d369d70e0aee0eb789e3
Doug Latornell <djl@douglatornell.ca>
2017-04-22 18:18:12 -0700

    Update make_readme.py module for research figures notebooks.

--- notebooks/figures/research/make_readme.py
+++ notebooks/figures/research/make_readme.py
@@ -22,20 +22,20 @@ import os
 import re
 
 
-NBVIEWER = 'http://nbviewer.jupyter.org/urls'
+NBVIEWER = 'https://nbviewer.jupyter.org'
 REPO = 'bitbucket.org/salishsea/salishseanowcast/raw/tip'
 REPO_DIR = 'notebooks/figures/research'
 TITLE_PATTERN = re.compile('#{1,6} ?')
 
 
 def main():
-    url = os.path.join(NBVIEWER, REPO, REPO_DIR)
-    readme = """\
+    url = os.path.join(NBVIEWER, 'urls', REPO, REPO_DIR)
+    readme = f"""\
 The Jupyter Notebooks in this directory are for development and testing of
 the results figures generation modules of the Salish Sea model nowcast system.
 
 The links below are to static renderings of the notebooks via
-[nbviewer.jupyter.org](http://nbviewer.jupyter.org/).
+[nbviewer.jupyter.org]({NBVIEWER}).
 Descriptions under the links below are from the first cell of the notebooks
 (if that cell contains Markdown or raw text).
 
@@ -51,7 +51,7 @@ by the Salish Sea MEOPAR Project Contributors
 and The University of British Columbia.
 
 They are licensed under the Apache License, Version 2.0.
-http://www.apache.org/licenses/LICENSE-2.0
+https://www.apache.org/licenses/LICENSE-2.0
 Please see the LICENSE file for details of the license.
 """
     with open('README.md', 'wt') as f:
