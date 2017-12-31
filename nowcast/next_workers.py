ca7d1a5d6bb6b476a02193a950dac3b8d9e88c37
Doug Latornell <dlatornell@eoas.ubc.ca>
2017-12-31 13:07:35 -0800

    Re-enable upload_forcing for all run types.
    re: improvements in LiveOcean boundary condition file calculations.

--- nowcast/next_workers.py
+++ nowcast/next_workers.py
@@ -187,8 +187,7 @@ def after_grib_to_netcdf(msg, config, checklist):
     for host in config['run']['enabled hosts']:
         if not config['run']['enabled hosts'][host]['shared storage']:
             for msg_suffix, run_type in msg_run_type_mapping.items():
-#                if run_type in config['run types']:
-                if run_type in 'forecast2':
+                if run_type in config['run types']:
                     next_workers[f'success {msg_suffix}'].append(
                         NextWorker(
                             'nowcast.workers.upload_forcing',
