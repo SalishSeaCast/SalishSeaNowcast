8c794536294fc3a727a9f47367b169f04758b1c4
Doug Latornell <djl@douglatornell.ca>
2018-02-18 15:34:58 -0800

    Hook watch_NEMO_hindcast into automation.

--- nowcast/next_workers.py
+++ nowcast/next_workers.py
@@ -644,6 +644,32 @@ def after_watch_NEMO(msg, config, checklist):
     return next_workers[msg.type]
 
 
+def after_watch_NEMO_hindcast(msg, config, checklist):
+    """Calculate the list of workers to launch after the watch_NEMO_handcast
+    worker ends.
+
+    :arg msg: Nowcast system message.
+    :type msg: :py:class:`nemo_nowcast.message.Message`
+
+    :arg config: :py:class:`dict`-like object that holds the nowcast system
+                 configuration that is loaded from the system configuration
+                 file.
+    :type config: :py:class:`nemo_nowcast.config.Config`
+
+    :arg dict checklist: System checklist: data structure containing the
+                         present state of the nowcast system.
+
+    :returns: Worker(s) to launch next
+    :rtype: list
+    """
+    next_workers = {
+        'crash': [],
+        'failure': [],
+        'success': [],
+    }
+    return []
+
+
 def after_make_fvcom_boundary(msg, config, checklist):
     """Calculate the list of workers to launch after the
     after_make_fvcom_boundary worker ends.
