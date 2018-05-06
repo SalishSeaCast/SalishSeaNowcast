92c647713e04cd040ad3a1cbc1896ab4897e55ab
Doug Latornell <dlatornell@eoas.ubc.ca>
2018-05-06 15:51:02 -0400

    Add ssh_exec_command utility function & SSHCommandError exception.
    Also improve docstrings.

--- nowcast/ssh_sftp.py
+++ nowcast/ssh_sftp.py
@@ -21,6 +21,23 @@ import paramiko
 from nowcast import lib
 
 
+class SSHCommandError(Exception):
+    """Raised when :py:func:`nowcast.ssh_sftp.ssh_exec_command` result in
+    stderr output.
+
+    :param str cmd: Command that was executed via ssh on remote host.
+
+    :param str stdout: stdout from command execution.
+
+    :param str stderr: stderr from command execution.
+    """
+
+    def __init__(self, cmd, stdout, stderr):
+        self.cmd = cmd
+        self.stdout = stdout
+        self.stderr = stderr
+
+
 def ssh(host, key_filename, ssh_config_file='~/.ssh/config'):
     """Return an SSH client connected to host.
 
@@ -31,14 +48,15 @@ def ssh(host, key_filename, ssh_config_file='~/.ssh/config'):
     The client's close() method should be called when its usefulness
     had ended.
 
-    :arg host: Name of the host to connect the client to.
+    :param host: Name of the host to connect the client to.
     :type config: str
 
-    :arg ssh_config_file: File path/name of the SSH2 config file to obtain
-                     the hostname and username values.
-    :type ssh_config_file: str
+    :param str ssh_config_file: File path/name of the SSH2 config file from
+                                which to obtain the hostname and username
+                                values.
 
-    :returns: :py:class:`paramiko.client.SSHClient` object
+    :returns: ssh client object
+    :rtype: :py:class:`paramiko.client.SSHClient`
     """
     ssh_client = paramiko.client.SSHClient()
     ssh_client.load_system_host_keys()
@@ -55,6 +73,31 @@ def ssh(host, key_filename, ssh_config_file='~/.ssh/config'):
     return ssh_client
 
 
+def ssh_exec_command(ssh_client, cmd, host, logger):
+    """Execute cmd on host via ssh_client connection.
+
+    :param :py:class:`paramiko.client.SSHClient`
+
+    :param str cmd: Command to execute on host
+
+    :param str host: Name of the host to execute cmd on.
+
+    :param logger: Logger object to send debug messages to.
+    :type logger: :py:class:`logging.Logger`
+
+    :return: stdout that results from execution of cmd on host.
+    :rtype: str with newline separators
+
+    :raises: :py:class:`nowcast.ssh_sftp.SSHError`
+    """
+    _, _stdout, _stderr = ssh_client.exec_command(cmd)
+    logger.debug(f'executed {cmd} on {host}')
+    stderr = _stderr.read().decode()
+    if stderr:
+        raise SSHCommandError(cmd, _stdout.read().decode(), stderr)
+    return _stdout.read().decode()
+
+
 def sftp(host, key_filename, ssh_config_file='~/.ssh/config'):
     """Return an SFTP client connected to host, and the SSH client on
     which it is based.
@@ -66,36 +109,37 @@ def sftp(host, key_filename, ssh_config_file='~/.ssh/config'):
     The clients' close() methods should be called when their usefulness
     had ended.
 
-    :arg host: Name of the host to connect the client to.
+    :param host: Name of the host to connect the client to.
     :type config: str
 
-    :arg ssh_config_file: File path/name of the SSH2 config file to obtain
-                     the hostname and username values.
-    :type ssh_config_file: str
+    :param str ssh_config_file: File path/name of the SSH2 config file from
+                                which to obtain the hostname and username
+                                values.
 
-    :returns: 2-tuple containing a :class:`paramiko.client.SSHClient`
-              object and a :class:`paramiko.sftp_client.SFTPClient` object.
+    :returns: 2-tuple containing a ssh and sftp client objects
+    :rtype: (:py:class:`paramiko.client.SSHClient`,
+             :py:class:`paramiko.sftp_client.SFTPClient`)
     """
     ssh_client = ssh(host, key_filename, ssh_config_file)
     sftp_client = ssh_client.open_sftp()
     return ssh_client, sftp_client
 
 
-def upload_file(sftp_client, host_name, localpath, remotepath, logger):
+def upload_file(sftp_client, host, localpath, remotepath, logger):
     """Upload the file at localpath to remotepath on host_name via SFTP.
 
-    :arg sftp_client: SFTP client instance to use for upload.
+    :param sftp_client: SFTP client instance to use for upload.
     :type sftp_client: :py:class:`paramiko.sftp_client.SFTPClient`
 
-    :arg str host_name: Name of the host to upload the file to.
+    :param str host: Name of the host to upload the file to.
 
-    :arg localpath: Local path and file name of file to upload.
+    :param localpath: Local path and file name of file to upload.
     :type localpath: :py:class:`pathlib.Path`
 
-    :arg remotepath: Path and file name to upload file to on remote host.
+    :param remotepath: Path and file name to upload file to on remote host.
     :type localpath: :py:class:`pathlib.Path`
 
-    :arg logger: Logger object to send debug message to.
+    :param logger: Logger object to send debug message to.
     :type logger: :py:class:`logging.Logger`
     """
     sftp_client.put(os.fspath(localpath), os.fspath(remotepath))
@@ -108,4 +152,4 @@ def upload_file(sftp_client, host_name, localpath, remotepath, logger):
         # We're probably trying to change permissions on a file owned by
         # another user. We can live with not being able to do that.
         pass
-    logger.debug(f'{localpath} uploaded to {host_name} at {remotepath}')
+    logger.debug(f'{localpath} uploaded to {host} at {remotepath}')
