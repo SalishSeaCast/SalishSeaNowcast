4c355cd1d995de864486966a9427bdaf1ebf59ec
Doug Latornell <djl@douglatornell.ca>
2018-09-17 08:29:35 -0700

    Add script to tag NEMO code & config repos for production releases.

---
+++ release_mgmt/tag_release.py
@@ -0,0 +1,110 @@
+# Copyright 2013-2018 The Salish Sea MEOPAR Contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""SalishSeaCast release management script that tags NEMO code and model
+configuration repos for a production release.
+"""
+import argparse
+import logging
+from pathlib import Path
+
+import hglib
+import yaml
+
+NAME = 'tag_release'
+logger = logging.getLogger(NAME)
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    parsed_args = _command_line_interface()
+    repos = _load_repos_list(parsed_args.repos_file)
+    for repo in repos:
+        _tag_repo(repo, parsed_args.tag)
+
+
+def _command_line_interface():
+    parser = argparse.ArgumentParser(
+        description='''
+        Tag NEMO code and model configuration repos for a production release.
+        '''
+    )
+    parser.prog = f'python -m release_mgmt.{NAME}'
+    parser.add_argument(
+        'repos_file',
+        type=Path,
+        help='''
+        Path/name of YAML file containing the list of repository paths to tag.
+        The list is unique for each user and machine combination.
+        '''
+    )
+    parser.add_argument(
+        'tag',
+        help='''
+        Tag to mark the most recently committed repo changesets with.
+        Format: PROD-run-type-hypenated-description
+        Examples: PROD-hindcast-201806-v2-2017, PROD-nowcast-green-201702v2
+        '''
+    )
+    return parser.parse_args()
+
+
+def _load_repos_list(repos_file):
+    """
+    :param pathlib.Path repos_file:
+    :rtype: dict
+    """
+    with repos_file.open('rt') as f:
+        repos_info =  yaml.safe_load(f)
+    logger.info(f'read list of repos to tag from {repos_file}')
+    return (Path(repo) for repo in repos_info['repos'])
+
+
+def _tag_repo(repo, tag):
+    logger.info(f'processing repo: {repo}')
+    if not repo.exists():
+        logger.error(f'repo does not exist: {repo}')
+        raise SystemExit(2)
+    try:
+        with hglib.open(repo) as hg:
+            parents = hg.parents()
+            status = hg.status(
+                modified=True,
+                added=True,
+                removed=True,
+                deleted=True,
+                copies=True
+            )
+    except hglib.error.ServerError:
+        logger.error(f'unable to find Mercurial repo root in: {repo}')
+        raise SystemExit(2)
+    if status:
+        logger.error(f'uncommitted changes in repo: {repo}')
+        raise SystemExit(2)
+    with hglib.open(repo) as hg:
+        incoming_changes = hg.incoming()
+        if incoming_changes:
+            logger.warning(
+                f'found incoming changes from Bitbucket that you need to '
+                f'deal with in repo: {repo}'
+            )
+            raise SystemExit(2)
+        hg.tag(tag.encode(), message=f'Tag production release {tag}.')
+        logger.info(f'added {tag} to repo: {repo}')
+        hg.push()
+        logger.info(f'pushed {tag} tag to Bitbucket from repo: {repo}')
+
+
+if __name__ == '__main__':
+    main()  # pragma: no cover
---
+++ tests/release_mgmt/test_tag_release.py
@@ -0,0 +1,107 @@
+# Copyright 2013-2018 The Salish Sea MEOPAR Contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for SalishSeaCast tag_release script.
+"""
+from pathlib import Path
+from unittest.mock import call, Mock, patch
+
+import hglib
+import pytest
+
+from release_mgmt import tag_release
+
+
+@patch('release_mgmt.tag_release.logger', autospec=True)
+@patch(
+    'release_mgmt.tag_release.Path.exists', return_value=True, autospec=True
+)
+@patch('release_mgmt.tag_release.hglib.open', autospec=True)
+class TestTagRepo:
+    """Unit tests for _rag_repo() function.
+    """
+
+    def test_repo_not_exists(self, m_hg_open, m_exists, m_logger):
+        m_exists.return_value = False
+        with pytest.raises(SystemExit):
+            tag_release._tag_repo(
+                Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+            )
+        m_logger.error.assert_called_once_with(
+            'repo does not exist: NEMO-3.6-code'
+        )
+
+    def test_no_repo_root(self, m_hg_open, m_exists, m_logger):
+        m_hg_open.side_effect = hglib.error.ServerError
+        with pytest.raises(SystemExit):
+            tag_release._tag_repo(
+                Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+            )
+        m_logger.error.assert_called_once_with(
+            'unable to find Mercurial repo root in: NEMO-3.6-code'
+        )
+
+    def test_uncommitted_changes(self, m_hg_open, m_exists, m_logger):
+        m_hg_open().__enter__ = Mock(name='hg_client')
+        m_hg_open().__enter__.status.return_value = None
+        with pytest.raises(SystemExit):
+            tag_release._tag_repo(
+                Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+            )
+        m_logger.error.assert_called_once_with(
+            'uncommitted changes in repo: NEMO-3.6-code'
+        )
+
+    def test_incoming_changes(self, m_hg_open, m_exists, m_logger):
+        m_hg_open().__enter__ = Mock(name='hg_client')
+        m_hg_open().__enter__().status.return_value = None
+        m_hg_open().__enter__().incoming.return_value = ['changesets']
+        with pytest.raises(SystemExit):
+            tag_release._tag_repo(
+                Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+            )
+        m_logger.warning.assert_called_once_with(
+            'found incoming changes from Bitbucket that you need to deal '
+            'with in repo: NEMO-3.6-code'
+        )
+
+    def test_hg_tag(self, m_hg_open, m_exists, m_logger):
+        m_hg_client = m_hg_open().__enter__ = Mock(name='hg_client')
+        m_hg_client().status.return_value = None
+        m_hg_client().incoming.return_value = []
+        tag_release._tag_repo(
+            Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+        )
+        expected = 'PROD-hindcast-201806-v2-2017'.encode()
+        assert m_hg_client().tag.call_args_list[0][0][0] == expected
+        expected = {
+            'message': 'Tag production release PROD-hindcast-201806-v2-2017.'
+        }
+        assert m_hg_client().tag.call_args_list[0][1] == expected
+        assert m_logger.info.call_args_list[1] == call(
+            'added PROD-hindcast-201806-v2-2017 to repo: NEMO-3.6-code'
+        )
+
+    def test_hg_push(self, m_hg_open, m_exists, m_logger):
+        m_hg_client = m_hg_open().__enter__ = Mock(name='hg_client')
+        m_hg_client().status.return_value = None
+        m_hg_client().incoming.return_value = []
+        tag_release._tag_repo(
+            Path('NEMO-3.6-code'), 'PROD-hindcast-201806-v2-2017'
+        )
+        m_hg_client().push.assert_called_once_with()
+        assert m_logger.info.call_args_list[2] == call(
+            'pushed PROD-hindcast-201806-v2-2017 tag to Bitbucket from repo: '
+            'NEMO-3.6-code'
+        )
