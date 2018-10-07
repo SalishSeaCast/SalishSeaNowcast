fb741b849611c77308deeddf4123ca5460b1c9db
Doug Latornell <djl@douglatornell.ca>
2018-10-07 10:17:46 -0700

    Refactor watch_NEMO_hindcast interact with the HPC job via a class.

--- tests/workers/test_watch_NEMO_hindcast.py
+++ tests/workers/test_watch_NEMO_hindcast.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 """Unit tests for SalishSeaCast watch_NEMO_hindcast worker.
 """
+from pathlib import Path
 from types import SimpleNamespace
 from unittest.mock import patch, Mock
 
@@ -103,47 +104,12 @@ class TestFailure:
     return_value=(Mock(name="ssh_client"), Mock(name="sftp_client")),
     autospec=True,
 )
-@patch(
-    "nowcast.workers.watch_NEMO_hindcast._get_run_id",
-    return_value=("9813234", "01jul18hindcast"),
-    autospec=True,
-)
-@patch(
-    "nowcast.workers.watch_NEMO_hindcast._is_queued", return_value=False, autospec=True
-)
-@patch(
-    "nowcast.workers.watch_NEMO_hindcast._get_tmp_run_dir",
-    return_value="tmp_run_dir",
-    autospec=True,
-)
-@patch(
-    "nowcast.workers.watch_NEMO_hindcast._get_run_info",
-    return_value=SimpleNamespace(),
-    autospec=True,
-)
-@patch(
-    "nowcast.workers.watch_NEMO_hindcast._is_running", return_value=False, autospec=True
-)
+@patch("nowcast.workers.watch_NEMO_hindcast._HindcastJob", spec=True)
 class TestWatchNEMO_Hindcast:
     """Unit test for watch_NEMO_hindcast() function.
     """
 
-    @patch(
-        "nowcast.workers.watch_NEMO_hindcast._get_completion_state",
-        return_value="completed",
-        autospec=True,
-    )
-    def test_run_completed(
-        self,
-        m_get_completion_state,
-        m_is_running,
-        m_get_run_info,
-        m_get_tmp_run_dir,
-        m_is_queued,
-        m_get_run_id,
-        m_sftp,
-        m_logger,
-    ):
+    def test_run_completed(self, m_job, m_sftp, m_logger):
         parsed_args = SimpleNamespace(host_name="cedar", run_id=None)
         config = {
             "run": {
@@ -156,6 +122,12 @@ class TestWatchNEMO_Hindcast:
                 }
             }
         }
+        m_job().host_name = "cedar"
+        m_job().job_id, m_job().run_id = "9813234", "01jul18hindcast"
+        m_job().is_queued.return_value = False
+        m_job().tmp_run_dir = Path("tmp_run_dir")
+        m_job().is_running.return_value = False
+        m_job().get_completion_state.return_value = "completed"
         checklist = watch_NEMO_hindcast.watch_NEMO_hindcast(parsed_args, config)
         expected = {
             "hindcast": {
@@ -168,20 +140,7 @@ class TestWatchNEMO_Hindcast:
         assert checklist == expected
 
     @pytest.mark.parametrize("completion_state", ["cancelled", "aborted"])
-    @patch("nowcast.workers.watch_NEMO_hindcast._get_completion_state", autospec=True)
-    def test_run_cancelled_or_aborted(
-        self,
-        m_get_completion_state,
-        m_is_running,
-        m_get_run_info,
-        m_get_tmp_run_dir,
-        m_is_queued,
-        m_get_run_id,
-        m_sftp,
-        m_logger,
-        completion_state,
-    ):
-        m_get_completion_state.return_value = completion_state
+    def test_run_cancelled_or_aborted(self, m_job, m_sftp, m_logger, completion_state):
         parsed_args = SimpleNamespace(host_name="cedar", run_id=None)
         config = {
             "run": {
@@ -194,5 +153,11 @@ class TestWatchNEMO_Hindcast:
                 }
             }
         }
+        m_job().host_name = "cedar"
+        m_job().job_id, m_job().run_id = "9813234", "01jul18hindcast"
+        m_job().is_queued.return_value = False
+        m_job().tmp_run_dir = Path("tmp_run_dir")
+        m_job().is_running.return_value = False
+        m_job().get_completion_state.return_value = completion_state
         with pytest.raises(nemo_nowcast.WorkerError):
             watch_NEMO_hindcast.watch_NEMO_hindcast(parsed_args, config)
