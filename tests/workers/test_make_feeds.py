d628ca1b66cede9e82bbdb3972966c959f8ad467
Doug Latornell <djl@douglatornell.ca>
2019-02-26 14:55:05 -0800

    Update make_feeds unit test re: updated version of feedgen pkg.

--- tests/workers/test_make_feeds.py
+++ tests/workers/test_make_feeds.py
@@ -203,7 +203,7 @@ class TestGenerateFeed:
             '  <link href="https://salishsea.eos.ubc.ca/storm-surge/'
             'forecast.html" rel="related" type="text/html"/>',
             '  <generator uri="http://lkiesow.github.io/python-feedgen" '
-            'version="0.6.1">python-feedgen</generator>',
+            'version="0.7.0">python-feedgen</generator>',
             "  <rights>Copyright 2015-2016, Salish Sea MEOPAR Project Contributors "
             "and The University of British Columbia</rights>",
             "</feed>",
