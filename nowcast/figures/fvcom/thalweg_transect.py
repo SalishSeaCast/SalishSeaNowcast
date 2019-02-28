eb85bbb15fef3bd53be23b1699fb26c5579912b3
Doug Latornell <dlatornell@eoas.ubc.ca>
2019-02-28 12:16:43 -0800

    Change Axes.set_axes_bgcolor() calls to Axes.set_facecolor().
    re: matplotlib>=3; see issue #62

--- nowcast/figures/fvcom/thalweg_transect.py
+++ nowcast/figures/fvcom/thalweg_transect.py
@@ -67,7 +67,7 @@ def make_figure(
 def _prep_fig_axes(figsize, theme):
     fig = plt.figure(figsize=figsize, facecolor=theme.COLOURS["figure"]["facecolor"])
     ax = fig.add_subplot(1, 1, 1)
-    ax.set_axis_bgcolor(theme.COLOURS["axes"]["background"])
+    ax.set_facecolor(theme.COLOURS["axes"]["background"])
     return fig, ax
 
 
