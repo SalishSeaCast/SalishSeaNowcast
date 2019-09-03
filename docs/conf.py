771fb4b933d1a126880ad3813e8f48b575d54478
Doug Latornell <djl@douglatornell.ca>
2019-09-03 11:32:13 -0700

    Add index & module index to docs w/ links in sidebar menu

--- docs/conf.py
+++ docs/conf.py
@@ -167,10 +167,13 @@ html_favicon = "_static/MEOPAR_favicon.ico"
 html_last_updated_fmt = "%b %d, %Y"
 
 # If false, no module index is generated.
-html_domain_indices = False
+html_domain_indices = True
 
 # If false, no index is generated.
-html_use_index = False
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = True
