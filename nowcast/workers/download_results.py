29c7f83b26fd0bb1a594be144c3056878c34ef5f
Doug Latornell <dlatornell@eoas.ubc.ca>
2019-10-30 17:06:05 -0400

    Refactor download_results.main() unit tests to use mock_nowcast_worker fixture

--- nowcast/workers/download_results.py
+++ nowcast/workers/download_results.py
@@ -12,8 +12,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Salish Sea NEMO nowcast worker that downloads the results files
-from a run on the HPC/cloud facility to archival storage.
+"""SalishSeaCast worker that downloads the results files from a run
+on an HPC/cloud facility to archival storage.
 """
 import contextlib
 import logging
@@ -59,6 +59,7 @@ def main():
         help="Date of the run to download results files from.",
     )
     worker.run(download_results, success, failure)
+    return worker
 
 
 def success(parsed_args):
