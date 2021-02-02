cb7412b078bdbb056666dd90a0a4e1a641bb61a0
Doug Latornell <dlatornell@eoas.ubc.ca>
2021-02-02 11:02:38 -0800

    Bump to v21.1.dev0 re: Happy New Year 2021!

--- nowcast/__init__.py
+++ nowcast/__init__.py
@@ -12,4 +12,4 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-__version__ = "20.1.dev0"
+__version__ = "21.1.dev0"
