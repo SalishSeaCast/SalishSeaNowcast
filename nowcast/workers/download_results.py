87658065e385139f5db046db7bf4c013c3788895
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-06-01 11:52:50 -0700

    Add missing SPDX license identifier comment

--- nowcast/workers/download_results.py
+++ nowcast/workers/download_results.py
@@ -12,6 +12,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
 """SalishSeaCast worker that downloads the results files from a run
 on an HPC/cloud facility to archival storage.
 """
