93f46ab4ebaa4fa67a9f24b5c95235864a0226d7
Doug Latornell <dlatornell@eoas.ubc.ca>
2023-02-10 12:28:00 -0800

    Update unit test re: river proportion dict module
    Missed in 8afcf09c.

--- tests/workers/test_make_runoff_file.py
+++ tests/workers/test_make_runoff_file.py
@@ -106,7 +106,7 @@ class TestConfig:
         rivers = prod_config["rivers"]
         assert rivers["file template"] == "R202108Dailies_{:y%Ym%md%d}.nc"
         assert rivers["rivers dir"] == "/results/forcing/rivers/"
-        assert rivers["prop_dict modules"]["b202108"] == "salishsea_tools.river_202108"
+        assert rivers["prop_dict module"] == "salishsea_tools.river_202108"
         assert (
             rivers["SOG river files"]["Capilano"]
             == "/opp/observations/rivers/Capilano/Caplilano_08GA010_day_avg_flow"
