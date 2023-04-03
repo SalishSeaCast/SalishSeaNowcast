b3f13bea7312ad805eef5ac78cf5c8af636f9a3a
Doug Latornell <dlatornell@eoas.ubc.ca>
2023-04-03 16:50:22 -0700

    Add crop_gribs worker implementation and unit tests

--- nowcast/workers/crop_gribs.py
+++ nowcast/workers/crop_gribs.py
@@ -24,8 +24,12 @@ new GRIB2 files.
 #
 # * https://github.com/SalishSeaCast/analysis-doug/tree/main/notebooks/continental-HRDPS/crop-grib-to-SSC-domain.ipynb.ipynb
 import logging
+import warnings
+from pathlib import Path
 
 import arrow
+import xarray
+from cfgrib import xarray_to_grib
 from nemo_nowcast import NowcastWorker
 
 
@@ -73,7 +77,92 @@ def crop_gribs(parsed_args, config, *args):
     and produces day-long NEMO atmospheric forcing netCDF files.
     """
     checklist = {}
+    forecast = parsed_args.forecast
+    fcst_date = parsed_args.fcst_date
+    logger.info(
+        f"cropping {fcst_date.format('YYYY-MM-DD')} ECCC HRDPS 2.5km continental "
+        f"{forecast}Z GRIB files to SalishSeaCast subdomain"
+    )
+    eccc_file_tmpl = config["weather"]["download"]["2.5 km"]["ECCC file template"]
+    ssc_file_tmpl = config["weather"]["download"]["2.5 km"]["SSC cropped file template"]
+    var_names = config["weather"]["download"]["2.5 km"]["variables"]
+    fcst_dur = config["weather"]["download"]["2.5 km"]["forecast duration"]
+    for msc_var, grib_var, _ in var_names:
+        eccc_grib_files = _calc_grib_file_paths(
+            eccc_file_tmpl, fcst_date, forecast, fcst_dur, msc_var, config
+        )
+        ssc_grib_files = _calc_grib_file_paths(
+            ssc_file_tmpl, fcst_date, forecast, fcst_dur, msc_var, config
+        )
+        _write_ssc_grib_files(
+            msc_var, grib_var, eccc_grib_files, ssc_grib_files, config
+        )
+    checklist[forecast] = "cropped to SalishSeaCast subdomain"
     return checklist
+
+
+def _calc_grib_file_paths(file_tmpl, fcst_date, fcst_hr, fcst_dur, msc_var, config):
+    """
+    :param str file_tmpl:
+    :param :py:class:`arrow.Arrow` fcst_date:
+    :param str fcst_hr:
+    :param int fcst_dur:
+    :param str msc_var:
+    :rtype: list
+    """
+    grib_dir = Path(config["weather"]["download"]["2.5 km"]["GRIB dir"])
+    fcst_yyyymmdd = fcst_date.format("YYYYMMDD")
+    grib_domain = "ECCC" if "SSC" not in file_tmpl else "SSC"
+    logger.debug(
+        f"creating {msc_var} {grib_domain} GRIB file paths list for "
+        f"{fcst_yyyymmdd} {fcst_hr}Z forecast"
+    )
+    grib_files = []
+    for fcst_step in range(1, fcst_dur + 1):
+        grib_hr_dir = grib_dir / Path(fcst_yyyymmdd, fcst_hr, f"{fcst_step:03d}")
+        grib_file = file_tmpl.format(
+            date=fcst_yyyymmdd,
+            forecast=fcst_hr,
+            variable=msc_var,
+            hour=f"{fcst_step:03d}",
+        )
+        grib_files.append(grib_hr_dir / grib_file)
+    return grib_files
+
+
+def _write_ssc_grib_files(msc_var, grib_var, eccc_grib_files, ssc_grib_files, config):
+    """
+    :param str msc_var:
+    :param str grib_var:
+    :param list eccc_grib_files:
+    :param list ssc_grib_files:
+    :param dict config:
+    """
+    y_min, y_max = config["weather"]["download"]["2.5 km"]["lat indices"]
+    x_min, x_max = config["weather"]["download"]["2.5 km"]["lon indices"]
+    # We need 1 point more than the final domain size to facilitate calculation of the
+    # grid rotation angle for the wind components
+    y_slice = slice(y_min, y_max + 1)
+    x_slice = slice(x_min, x_max + 1)
+    ny, nx = y_max - y_min + 1, x_max - x_min + 1
+    for eccc_grib_file, ssc_grib_file in zip(eccc_grib_files, ssc_grib_files):
+        with xarray.open_dataset(
+            eccc_grib_file, engine="cfgrib", backend_kwargs={"indexpath": ""}
+        ) as eccc_ds:
+            ssc_ds = eccc_ds.sel(y=y_slice, x=x_slice)
+        ssc_ds[grib_var].attrs.update(
+            {
+                "GRIB_numberOfPoints": nx * ny,
+                "GRIB_Nx": nx,
+                "GRIB_Ny": ny,
+            }
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            xarray_to_grib.to_grib(ssc_ds, ssc_grib_file)
+        logger.debug(
+            f"wrote {msc_var} GRIB file cropped to SalishSeaCast subdomain: {ssc_grib_file}"
+        )
 
 
 if __name__ == "__main__":
--- tests/workers/test_crop_gribs.py
+++ tests/workers/test_crop_gribs.py
@@ -41,6 +41,26 @@ def config(base_config):
                 weather:
                   download:
                     2.5 km:
+                      GRIB dir: forcing/atmospheric/continental2.5/GRIB/
+                      ECCC file template: "{date}T{forecast}Z_MSC_HRDPS_{variable}_RLatLon0.0225_PT{hour}H.grib2"
+                      SSC cropped file template: "{date}T{forecast}Z_MSC_HRDPS_{variable}_RLatLon0.0225_PT{hour}H_SSC.grib2"
+                      SSC georef: forcing/atmospheric/continental2.5/GRIB/SSC_grid_georef.nc
+                      variables:
+                        # [MSC name, GRIB std name, NEMO name]
+                        - [UGRD_AGL-10m, u10, u_wind]           # u component of wind velocity at 10m elevation
+                        - [VGRD_AGL-10m, v10, v_wind]           # v component of wind velocity at 10m elevation
+                        - [DSWRF_Sfc, ssrd, solar]              # accumulated downward shortwave (solar) radiation at ground level
+                        - [DLWRF_Sfc, strd, therm_rad]          # accumulated downward longwave (thermal) radiation at ground level
+                        - [LHTFL_Sfc, lhtfl, LHTFL_surface]     # upward surface latent heat flux (for VHFR FVCOM)
+                        - [TMP_AGL-2m, t2m, tair]               # air temperature at 2m elevation
+                        - [SPFH_AGL-2m, sh2, qair]              # specific humidity at 2m elevation
+                        - [RH_AGL-2m, r2, RH_2maboveground]     # relative humidity at 2m elevation (for VHFR FVCOM)
+                        - [APCP_Sfc, unknown, precip]           # accumulated precipitation at ground level
+                        - [PRATE_Sfc, prate, PRATE_surface]     # precipitation rate at ground level (for VHFR FVCOM)
+                        - [PRMSL_MSL, prmsl, atmpres]           # atmospheric pressure at mean sea level
+                      lon indices: [300, 490]
+                      lat indices: [230, 460]
+                      forecast duration: 48  # hours
                 """
             )
         )
@@ -102,6 +122,40 @@ class TestConfig:
             "crash",
         ]
 
+    def test_weather_download_2_5_km_section(self, prod_config):
+        weather_download = prod_config["weather"]["download"]["2.5 km"]
+        assert (
+            weather_download["GRIB dir"]
+            == "/results/forcing/atmospheric/continental2.5/GRIB/"
+        )
+        assert (
+            weather_download["ECCC file template"]
+            == "{date}T{forecast}Z_MSC_HRDPS_{variable}_RLatLon0.0225_PT{hour}H.grib2"
+        )
+        assert (
+            weather_download["SSC cropped file template"]
+            == "{date}T{forecast}Z_MSC_HRDPS_{variable}_RLatLon0.0225_PT{hour}H_SSC.grib2"
+        )
+        assert (
+            weather_download["SSC georef"]
+            == "/results/forcing/atmospheric/continental2.5/GRIB/SSC_grid_georef.nc"
+        )
+        assert weather_download["variables"] == [
+            ["UGRD_AGL-10m", "u10", "u_wind"],
+            ["VGRD_AGL-10m", "v10", "v_wind"],
+            ["DSWRF_Sfc", "ssrd", "solar"],
+            ["DLWRF_Sfc", "strd", "therm_rad"],
+            ["LHTFL_Sfc", "lhtfl", "LHTFL_surface"],
+            ["TMP_AGL-2m", "t2m", "tair"],
+            ["SPFH_AGL-2m", "sh2", "qair"],
+            ["RH_AGL-2m", "r2", "RH_2maboveground"],
+            ["APCP_Sfc", "unknown", "precip"],
+            ["PRATE_Sfc", "prate", "PRATE_surface"],
+            ["PRMSL_MSL", "prmsl", "atmpres"],
+        ]
+        assert weather_download["lon indices"] == [300, 490]
+        assert weather_download["lat indices"] == [230, 460]
+
 
 @pytest.mark.parametrize(
     "forecast, forecast_date",
@@ -157,9 +211,160 @@ class TestFailure:
         assert msg_type == f"failure {forecast}"
 
 
+@pytest.mark.parametrize("forecast", ("00", "06", "12", "18"))
 class TestCropGribs:
     """Unit tests for crop_gribs() function."""
 
-    def test_crop_gribs(self):
-        # TODO: test something
-        pass
+    @staticmethod
+    @pytest.fixture
+    def mock_write_ssc_grib_files(monkeypatch):
+        def _mock_write_ssc_grib_files(
+            msc_var, grib_var, eccc_grib_files, ssc_grib_files, config
+        ):
+            pass
+
+        monkeypatch.setattr(
+            crop_gribs, "_write_ssc_grib_files", _mock_write_ssc_grib_files
+        )
+
+    def test_checklist(self, forecast, mock_write_ssc_grib_files, config, caplog):
+        parsed_args = SimpleNamespace(
+            forecast=forecast, fcst_date=arrow.get("2023-04-02")
+        )
+        caplog.set_level(logging.DEBUG)
+
+        checklist = crop_gribs.crop_gribs(parsed_args, config)
+
+        expected = {forecast: "cropped to SalishSeaCast subdomain"}
+        assert checklist == expected
+
+    def test_log_messages(self, forecast, mock_write_ssc_grib_files, config, caplog):
+        parsed_args = SimpleNamespace(
+            forecast=forecast, fcst_date=arrow.get("2023-04-02")
+        )
+        caplog.set_level(logging.DEBUG)
+
+        crop_gribs.crop_gribs(parsed_args, config)
+
+        assert caplog.records[0].levelname == "INFO"
+        expected = (
+            f"cropping 2023-04-02 ECCC HRDPS 2.5km continental {forecast}Z GRIB files to "
+            f"SalishSeaCast subdomain"
+        )
+        assert caplog.messages[0] == expected
+
+
+class TestCalcGribFilePaths:
+    """Unit tests for _calc_grib_file_paths() function."""
+
+    def test_ECCC_grib_file_paths(self, config):
+        file_tmpl = config["weather"]["download"]["2.5 km"]["ECCC file template"]
+        fcst_date = arrow.get("2023-04-03")
+        fcst_hr = "12"
+        fcst_dur = 2
+        msc_var = "UGRD_AGL-10m"
+
+        grib_files = crop_gribs._calc_grib_file_paths(
+            file_tmpl, fcst_date, fcst_hr, fcst_dur, msc_var, config
+        )
+
+        expected = [
+            Path(
+                "forcing/atmospheric/continental2.5/GRIB/20230403/12/001/"
+                "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT001H.grib2"
+            ),
+            Path(
+                "forcing/atmospheric/continental2.5/GRIB/20230403/12/002/"
+                "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT002H.grib2"
+            ),
+        ]
+        assert grib_files == expected
+
+    def test_SSC_cropped_grib_file_paths(self, config):
+        file_tmpl = config["weather"]["download"]["2.5 km"]["SSC cropped file template"]
+        fcst_date = arrow.get("2023-04-03")
+        fcst_hr = "12"
+        fcst_dur = 2
+        msc_var = "UGRD_AGL-10m"
+
+        grib_files = crop_gribs._calc_grib_file_paths(
+            file_tmpl, fcst_date, fcst_hr, fcst_dur, msc_var, config
+        )
+
+        expected = [
+            Path(
+                "forcing/atmospheric/continental2.5/GRIB/20230403/12/001/"
+                "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT001H_SSC.grib2"
+            ),
+            Path(
+                "forcing/atmospheric/continental2.5/GRIB/20230403/12/002/"
+                "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT002H_SSC.grib2"
+            ),
+        ]
+        assert grib_files == expected
+
+    @pytest.mark.parametrize(
+        "grib_domain, file_tmpl_key",
+        (
+            ("ECCC", "ECCC file template"),
+            ("SSC", "SSC cropped file template"),
+        ),
+    )
+    def test_log_messages(self, grib_domain, file_tmpl_key, config, caplog):
+        file_tmpl = config["weather"]["download"]["2.5 km"][file_tmpl_key]
+        fcst_date = arrow.get("2023-04-03")
+        fcst_hr = "12"
+        fcst_dur = 2
+        msc_var = "UGRD_AGL-10m"
+        caplog.set_level(logging.DEBUG)
+
+        crop_gribs._calc_grib_file_paths(
+            file_tmpl, fcst_date, fcst_hr, fcst_dur, msc_var, config
+        )
+
+        assert caplog.records[0].levelname == "DEBUG"
+        expected = f"creating UGRD_AGL-10m {grib_domain} GRIB file paths list for 20230403 12Z forecast"
+        assert caplog.messages[0] == expected
+
+
+# class TestWriteSscGribFiles:
+#     """Unit tests for _write_ssc_grib_files() function."""
+#
+#     def test_log_messages(self, config, caplog, monkeypatch):
+#         def mock_open_dataset(path, engine, backend_kwargs):
+#             pass
+#
+#         monkeypatch.setattr(crop_gribs.xarray, "open_dataset", mock_open_dataset)
+#
+#         msc_var = "UGRD_AGL-10m"
+#         grib_var = "u10"
+#         eccc_grib_files = [
+#             Path(
+#                 "forcing/atmospheric/continental2.5/GRIB/20230403/12/001/"
+#                 "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT001H.grib2"
+#             ),
+#             Path(
+#                 "forcing/atmospheric/continental2.5/GRIB/20230403/12/002/"
+#                 "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT002H.grib2"
+#             ),
+#         ]
+#         ssc_grib_files = [
+#             Path(
+#                 "forcing/atmospheric/continental2.5/GRIB/20230403/12/001/"
+#                 "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT001H_SSC.grib2"
+#             ),
+#             Path(
+#                 "forcing/atmospheric/continental2.5/GRIB/20230403/12/002/"
+#                 "20230403T12Z_MSC_HRDPS_UGRD_AGL-10m_RLatLon0.0225_PT002H_SSC.grib2"
+#             ),
+#         ]
+#         caplog.set_level(logging.DEBUG)
+#
+#         crop_gribs._write_ssc_grib_files(msc_var, grib_var, eccc_grib_files, ssc_grib_files, config)
+#
+#         assert caplog.records[0].levelname == "DEBUG"
+#         expected = (
+#             f"wrote UGRD_AGL-10m GRIB file cropped to SalishSeaCast subdomain: "
+#             f""
+#         )
+#         assert caplog.messages[0] == expected
