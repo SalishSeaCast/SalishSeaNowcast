bee1d665fa3808900bda0c6390e97132fb80dce3
Doug Latornell <dlatornell@eoas.ubc.ca>
2023-05-26 14:50:48 -0700

    Add _calc_watershed_flows() and unit tests

--- nowcast/workers/make_v202111_runoff_file.py
+++ nowcast/workers/make_v202111_runoff_file.py
@@ -31,6 +31,36 @@ NAME = "make_v202111_runoff_file"
 logger = logging.getLogger(NAME)
 
 
+# TODO: Ideally these module-level variables should be in the main or a supplemental config file.
+#       They should also be in a better data structure.
+#       We're leaving them here for now because it's "good enough",
+#       and the priority is getting v202111 into production.
+watershed_names = [
+    "bute",
+    "evi_n",
+    "jervis",
+    "evi_s",
+    "howe",
+    "jdf",
+    "skagit",
+    "puget",
+    "toba",
+    "fraser",
+]
+rivers_for_watershed = {
+    "bute": {"primary": "Homathko_Mouth", "secondary": None},
+    "evi_n": {"primary": "Salmon_Sayward", "secondary": None},
+    "jervis": {"primary": "Clowhom_ClowhomLake", "secondary": "RobertsCreek"},
+    "evi_s": {"primary": "Englishman", "secondary": None},
+    "howe": {"primary": "Squamish_Brackendale", "secondary": None},
+    "jdf": {"primary": "SanJuan_PortRenfrew", "secondary": None},
+    "skagit": {"primary": "Skagit_MountVernon", "secondary": "Snohomish_Monroe"},
+    "puget": {"primary": "Nisqually_McKenna", "secondary": "Greenwater_Greenwater"},
+    "toba": {"primary": "Homathko_Mouth", "secondary": "Theodosia"},
+    "fraser": {"primary": "Fraser", "secondary": "Nicomekl_Langley"},
+}
+
+
 def main():
     """For command-line usage see:
 
@@ -62,9 +92,72 @@ def failure(parsed_args):
 
 
 def make_v202111_runoff_file(parsed_args, config, *args):
+    obs_date = parsed_args.data_date
+    logger.info(
+        f"calculating NEMO runoff forcing for 202108 bathymetry for {obs_date.format('YYYY-MM-DD')}"
+    )
+    flows = _calc_watershed_flows(obs_date, config)
+
     checklist = {}
 
     return checklist
+
+
+def _calc_watershed_flows(obs_date, config):
+    """
+    :param :py:class:`arrow.Arrow` obs_date:
+    :param dict config:
+
+    :rtype: dict
+    """
+
+    flows = {}
+    for watershed_name in watershed_names:
+        if watershed_name == "fraser":
+            flows["fraser"], flows["non_fraser"] = _do_fraser(obs_date, config)
+        else:
+            if rivers_for_watershed[watershed_name]["secondary"] is None:
+                logger.debug(f"no secondary river for {watershed_name} watershed")
+            flows[watershed_name] = _do_a_river_pair(
+                watershed_name,
+                obs_date,
+                rivers_for_watershed[watershed_name]["primary"],
+                config,
+                rivers_for_watershed[watershed_name]["secondary"],
+            )
+        logger.debug(
+            f"{watershed_name} watershed flow: {flows[watershed_name]:.3f} m3 s-1"
+        )
+    return flows
+
+
+def _do_fraser(obs_date, config):
+    """
+    :param :py:class:`arrow.Arrow` obs_date:
+    :param dict config:
+
+    :rtype: tuple
+    """
+    pass
+
+
+def _do_a_river_pair(
+    watershed_name,
+    obs_date,
+    primary_river_name,
+    config,
+    secondary_river_name=None,
+):
+    """
+    :param str watershed_name:
+    :param :py:class:`arrow.Arrow` obs_date:
+    :param str primary_river_name:
+    :param dict config:
+    :param str or :py:class:`NoneType` secondary_river_name:
+
+    :rtype: float
+    """
+    pass
 
 
 if __name__ == "__main__":
--- tests/workers/test_make_v202111_runoff_file.py
+++ tests/workers/test_make_v202111_runoff_file.py
@@ -98,6 +98,51 @@ class TestConfig:
         ]
 
 
+class TestModuleVariables:
+    """
+    Unit tests for config-type information that is stored in module variables in the
+    make_v202111_runoff_file worker.
+    """
+
+    def test_watershed_names(self):
+        expected = [
+            "bute",
+            "evi_n",
+            "jervis",
+            "evi_s",
+            "howe",
+            "jdf",
+            "skagit",
+            "puget",
+            "toba",
+            "fraser",
+        ]
+
+        assert make_v202111_runoff_file.watershed_names == expected
+
+    def test_rivers_for_watershed(self):
+        expected = {
+            "bute": {"primary": "Homathko_Mouth", "secondary": None},
+            "evi_n": {"primary": "Salmon_Sayward", "secondary": None},
+            "jervis": {"primary": "Clowhom_ClowhomLake", "secondary": "RobertsCreek"},
+            "evi_s": {"primary": "Englishman", "secondary": None},
+            "howe": {"primary": "Squamish_Brackendale", "secondary": None},
+            "jdf": {"primary": "SanJuan_PortRenfrew", "secondary": None},
+            "skagit": {
+                "primary": "Skagit_MountVernon",
+                "secondary": "Snohomish_Monroe",
+            },
+            "puget": {
+                "primary": "Nisqually_McKenna",
+                "secondary": "Greenwater_Greenwater",
+            },
+            "toba": {"primary": "Homathko_Mouth", "secondary": "Theodosia"},
+            "fraser": {"primary": "Fraser", "secondary": "Nicomekl_Langley"},
+        }
+
+        assert make_v202111_runoff_file.rivers_for_watershed == expected
+
+
 class TestSuccess:
     """Unit test for success() function."""
 
@@ -129,7 +174,19 @@ class TestFailure:
 class TestMakeV202111RunoffFile:
     """Unit tests for make_v202111_runoff_file() function."""
 
-    def test_checklist(self, config, caplog):
+    @staticmethod
+    @pytest.fixture
+    def mock_calc_watershed_flows(monkeypatch):
+        def _mock_calc_watershed_flows(obs_date, config):
+            pass
+
+        monkeypatch.setattr(
+            make_v202111_runoff_file,
+            "_calc_watershed_flows",
+            _mock_calc_watershed_flows,
+        )
+
+    def test_checklist(self, mock_calc_watershed_flows, config, caplog):
         parsed_args = SimpleNamespace(data_date=arrow.get("2023-05-19"))
 
         checklist = make_v202111_runoff_file.make_v202111_runoff_file(
@@ -137,3 +194,103 @@ class TestMakeV202111RunoffFile:
         )
 
         assert checklist == {}
+
+    def test_log_messages(self, mock_calc_watershed_flows, config, caplog):
+        parsed_args = SimpleNamespace(data_date=arrow.get("2023-05-26"))
+        caplog.set_level(logging.DEBUG)
+
+        make_v202111_runoff_file.make_v202111_runoff_file(parsed_args, config)
+
+        assert caplog.records[0].levelname == "INFO"
+        expected = (
+            "calculating NEMO runoff forcing for 202108 bathymetry for 2023-05-26"
+        )
+        assert caplog.messages[0] == expected
+
+
+class TestCalcWatershedFlows:
+    """Unit test for _calc_watershed_flows()."""
+
+    @staticmethod
+    @pytest.fixture
+    def mock_do_a_river_pair(monkeypatch):
+        mock_watershed_flows = [
+            ("bute", 83.3223456),
+            ("evi_n", 354.56739384),
+            ("jervis", 118.43897380000001),
+            ("evi_s", 175.51416120000002),
+            ("howe", 84.56446136),
+            ("jdf", 380.26035625),
+            ("skagit", 519.6378946),
+            ("puget", 442.39027494999993),
+            ("toba", 56.474250732),
+        ]
+
+        def _mock_do_a_river_pair(
+            watershed_name,
+            obs_date,
+            primary_river_name,
+            config,
+            secondary_river_name=None,
+        ):
+            return mock_watershed_flows.pop(0)[1]
+
+        monkeypatch.setattr(
+            make_v202111_runoff_file, "_do_a_river_pair", _mock_do_a_river_pair
+        )
+
+    @staticmethod
+    @pytest.fixture
+    def mock_do_fraser(monkeypatch):
+        def _mock_do_fraser(obs_date, config):
+            return 1094.5609129386, 63.9781423614
+
+        monkeypatch.setattr(make_v202111_runoff_file, "_do_fraser", _mock_do_fraser)
+
+    def test_calc_watershed_flows(
+        self, mock_do_a_river_pair, mock_do_fraser, config, monkeypatch
+    ):
+        obs_date = arrow.get("2023-02-19")
+
+        flows = make_v202111_runoff_file._calc_watershed_flows(obs_date, config)
+
+        expected = {
+            "bute": 83.3223456,
+            "evi_n": 354.56739384,
+            "jervis": 118.43897380000001,
+            "evi_s": 175.51416120000002,
+            "howe": 84.56446136,
+            "jdf": 380.26035625,
+            "skagit": 519.6378946,
+            "puget": 442.39027494999993,
+            "toba": 56.474250732,
+            "fraser": 1094.5609129386,
+        }
+        for name in make_v202111_runoff_file.watershed_names:
+            assert flows[name] == pytest.approx(expected[name])
+        assert flows["non_fraser"] == pytest.approx(63.9781423614)
+
+    def test_log_messages(
+        self, mock_do_a_river_pair, mock_do_fraser, config, caplog, monkeypatch
+    ):
+        obs_date = arrow.get("2023-02-26")
+        caplog.set_level(logging.DEBUG)
+
+        make_v202111_runoff_file._calc_watershed_flows(obs_date, config)
+
+        assert caplog.records[0].levelname == "DEBUG"
+        assert caplog.messages[0] == "no secondary river for bute watershed"
+        assert caplog.messages[1] == "bute watershed flow: 83.322 m3 s-1"
+        assert caplog.messages[2] == "no secondary river for evi_n watershed"
+        assert caplog.messages[3] == "evi_n watershed flow: 354.567 m3 s-1"
+        assert caplog.messages[4] == "jervis watershed flow: 118.439 m3 s-1"
+        assert caplog.messages[5] == "no secondary river for evi_s watershed"
+        assert caplog.messages[6] == "evi_s watershed flow: 175.514 m3 s-1"
+        assert caplog.messages[7] == "no secondary river for howe watershed"
+        assert caplog.messages[8] == "howe watershed flow: 84.564 m3 s-1"
+        assert caplog.messages[9] == "no secondary river for jdf watershed"
+        assert caplog.messages[10] == "jdf watershed flow: 380.260 m3 s-1"
+        assert caplog.messages[11] == "skagit watershed flow: 519.638 m3 s-1"
+        assert caplog.messages[12] == "puget watershed flow: 442.390 m3 s-1"
+        assert caplog.messages[13] == "toba watershed flow: 56.474 m3 s-1"
+        assert caplog.messages[14] == "fraser watershed flow: 1094.561 m3 s-1"
