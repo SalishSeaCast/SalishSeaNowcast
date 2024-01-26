8ccc7d9b56d278a432d8f78932f2a97a6e17b60c
Doug Latornell <dlatornell@eoas.ubc.ca>
2024-01-26 08:12:20 -0800

    Fix tests re: acad43d7283487

--- tests/workers/test_download_results.py
+++ tests/workers/test_download_results.py
@@ -253,6 +253,7 @@ class TestConfig:
     def test_results_archive(self, prod_config):
         archives = {
             "nowcast": "/results/SalishSea/nowcast-blue.202111/",
+            "nowcast-dev": "/results/SalishSea/nowcast-dev.201905/",
             "forecast": "/results/SalishSea/forecast.202111/",
             "forecast2": "/results/SalishSea/forecast2.202111/",
             "nowcast-green": "/results2/SalishSea/nowcast-green.202111/",
