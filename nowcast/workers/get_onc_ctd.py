509c2abf394686fceb8d62f170a812c296a37d83
Doug Latornell <dlatornell@eoas.ubc.ca>
2024-02-02 16:15:23 -0800

    Improve get_onc_ctd.main() function
    Update docstring re: issue #121.
    
    Return worker so that modernized unit tests for main() work; re issue #81.

--- nowcast/workers/get_onc_ctd.py
+++ nowcast/workers/get_onc_ctd.py
@@ -42,9 +42,7 @@ logger = logging.getLogger(NAME)
 
 
 def main():
-    """Set up and run the worker.
-
-    For command-line usage see:
+    """For command-line usage see:
 
     :command:`python -m nowcast.workers.get_onc_ctd -h`
     """
@@ -61,6 +59,7 @@ def main():
         help="UTC date to get ONC node CTD data for.",
     )
     worker.run(get_onc_ctd, success, failure)
+    return worker
 
 
 def success(parsed_args):
