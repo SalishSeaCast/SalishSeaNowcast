6cda3111e3794f9112602ea650cc0c18278e6880
Doug Latornell <dlatornell@eoas.ubc.ca>
2024-06-07 10:32:45 -0700

    Use netcdf4 to save dataset in make_current_wind_file
    Explicitly use netcdf4 as the engine for dataset writing. This avoids
    incompatibilities in the resulting file that arise if it is written using
    h5netcdf.

--- nowcast/workers/make_ww3_current_file.py
+++ nowcast/workers/make_ww3_current_file.py
@@ -175,12 +175,12 @@ def make_ww3_current_file(parsed_args, config, *args):
                 u_current.time_counter, lats, lons, u_current, v_current, datasets
             )
             logger.debug("created currents dataset")
-            ds.to_netcdf(os.fspath(nc_filepath))
             dask_scheduler = {
                 "scheduler": "processes",
                 "max_workers": 8,
             }
             ds.compute(**dask_scheduler)
+            ds.to_netcdf(nc_filepath, engine="netcdf4")
     logger.debug(f"stored currents forcing file: {nc_filepath}")
     checklist = {
         run_type: os.fspath(nc_filepath),
