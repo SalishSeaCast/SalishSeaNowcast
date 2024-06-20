2a3b6c8203007998a82016bb44e9ea4ba9e68c80
Doug Latornell <dlatornell@eoas.ubc.ca>
2024-06-20 12:25:16 -0700

    Handle ValueError in get_onc_ctd salinity resampling
    xarray version 2023.05.0 introduced a ValueError exception raised when a
    DataArray being resampled is empty. We previously handled that condition by
    catching an IndexError. Now we catch ValueError too.
    
    The change to ValueError for an empty salinity DataArray didn't show up in
    production until the 14jun24 update of the production environment brought xarray
    version 2024.06.0 into use.

--- nowcast/workers/get_onc_ctd.py
+++ nowcast/workers/get_onc_ctd.py
@@ -176,7 +176,7 @@ def _create_dataset(onc_station, temperature, salinity):
         salinity_mean = salinity.resample(time="15Min").mean()
         salinity_std_dev = salinity.resample(time="15Min").std()
         salinity_sample_count = salinity.resample(time="15Min").count()
-    except IndexError:
+    except (IndexError, ValueError):
         logger.warning(f"no {onc_station} salinity data")
         salinity_mean = temperature_mean.copy()
         salinity_mean.name = "salinity"
