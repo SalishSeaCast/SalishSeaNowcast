a923c7c073fb46fbe679d68eb063dd6e154e59cc
Doug Latornell <dlatornell@eoas.ubc.ca>
2025-09-24 09:43:17 -0700

    Add missing SPDX short-form license identifier
    Inserted the `SPDX-License-Identifier: Apache-2.0` comment at the top of the
    `make_forcing_links.py` file. This communicates license information in portable
    and machine-readable manner.

--- nowcast/workers/make_forcing_links.py
+++ nowcast/workers/make_forcing_links.py
@@ -12,6 +12,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
 """SalishSeaCast worker that creates forcing files symlinks for NEMO runs.
 
 Create the forcing file symlinks for a nowcast run on the HPC/cloud
