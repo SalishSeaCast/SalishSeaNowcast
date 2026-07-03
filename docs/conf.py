880643cd65a3f297f3b448994d791946f5c7332d
Doug Latornell <dlatornell@eoas.ubc.ca>
2026-07-03 15:35:27 -0700

    Configure `sphinx_copybutton` options in `conf.py`
    Added customization options for the `sphinx_copybutton` extension:
    * Set prompt text regex
    * Respect bash line continuation character
    * Add a `no-copybutton` class that can be used to suppress the copy button

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,15 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+# -- Options for sphinx_copybutton extension ------------------------------
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+# Add a `no-copybutton` class that can be used to suppress the copy button
+copybutton_selector = "div:not(.no-copybutton) > div.highlight > pre"
+
+
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "nemonowcast": ("https://nemo-nowcast.readthedocs.io/en/latest/", None),
