41317758ce88bc7c030a9571aaab848570db473a
Doug Latornell <dlatornell@eoas.ubc.ca>
2026-08-14 18:47:11 -0700

    Update `salishsea_cmd` to use abs Pixi path
    Replaced relative `pixi` command paths with the absolute path to ensure
    compatibility with environments where `pixi` is not in the default `PATH`.
    Updated the command in `nowcast.yaml` and associated test cases.

--- tests/workers/test_run_ww3.py
+++ tests/workers/test_run_ww3.py
@@ -153,9 +153,7 @@ class TestConfig:
 
     def test_salishsea_cmd(self, prod_config):
         salishsea_cmd = prod_config["wave forecasts"]["salishsea cmd"]
-        expected = (
-            "pixi run -m /nemoShare/MEOPAR/nowcast-sys/SalishSeaNowcast salishsea"
-        )
+        expected = "/home/ubuntu/.pixi/bin/pixi run -m /nemoShare/MEOPAR/nowcast-sys/SalishSeaNowcast salishsea"
         assert salishsea_cmd == expected
 
 
